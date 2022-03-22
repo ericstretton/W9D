eb48f5079ff30732c0fec861e93ae894896980bb
ericstretton <eric.stretton@gmail.com>
2022-03-21 20:18:58 -0600

    Function 1 working

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Create three functions and test them by calling each one three times with different arguments and console logging the returned value
+
+// function #1: take 2 arg expected to be integers. Function adds the two arg together -->
+// then, multiple the result by five.
+
+function math(num1, num2){
+    var mathedNumber = ((num1 + num2)*5);
+    return mathedNumber;
+}
+
+
+
+
+
+var num1 = 2;
+
+var num2 = 13;
+
+var result = math(num1, num2);
+console.log(result);
+
+// Function #2 will take in 1 argument expected to be a string. The function will return true if -->
+// the string length is > 10 characters and false otherwise.
+
